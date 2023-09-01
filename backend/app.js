657d0b36bd2591c69a892588763faf1db9d50b15
Main <gimenezfrancotomas96@gmail.com>
2023-09-01 00:04:42 -0300

    eliminacion de dependencias

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,19 @@
 import { config } from 'dotenv'
-import express from 'express'
+import express, { json } from 'express'
 import { helmet } from 'helmet'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
 import { usersRoutes } from './src/routes/users.routes.js'
 
 config()
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const app = express()
-app.use(express.json())
-app.use(bodyParser.json())
+app.use(json())
 app.use(helmet)
 
 app.use('/api/users', usersRoutes())
 app.all('*', (req, res) => {
   res.status(404).send({ status: 'ERR', data: 'No se encuentra el endpoint solicitado' })
 })
+
 app.listen(PORT, async () => {
-  try {
-    await mongoose.connect(MONGODB_URI)
-    console.log(`Backend inicializado puerto ${PORT}`)
-  } catch (err) {
-    console.error(err.message)
-  }
+  console.log(`Backend inicializado puerto ${PORT}`)
 })
